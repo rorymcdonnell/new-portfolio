98d16181349b62208b1ba94eab62262d13589efa
Rory <rorymcdonnell95@gmail.com>
2023-04-24 15:33:33 +0200

    fixed animation causing scorllbar at project description

--- src/scenes/experience/index.tsx
+++ src/scenes/experience/index.tsx
@@ -104,7 +104,7 @@ const Experiences = ({ setSelectedPage }: Props) => {
                   viewport={{ once: true, amount: 0.5 }}
                   transition={{ duration: 0.5 }}
                   variants={{
-                    hidden: { opacity: 0, x: 50 },
+                    hidden: { opacity: 0, x: -50 },
                     visible: { opacity: 1, x: 0 },
                   }}
                 >
